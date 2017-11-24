59a156d2e12746cce3fb0b43b76caf972cdef869
Theo Moura <theomouram@gmail.com>
2017-11-23 21:06:08 -0300

    created service layer/ examples of http routes and LINQ

--- CTBack/CorreiosTrack/CorreiosTrack/Controllers/RastreiosController.cs
+++ CTBack/CorreiosTrack/CorreiosTrack/Controllers/RastreiosController.cs
@@ -1,42 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using System.Data;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
 using CorreiosTrack.Models;
 
 namespace CorreiosTrack.Controllers
 {
+    using CorreiosTrack.Services;
+    using System.Collections.Generic;
     using System.IO;
     using System.Xml;
-    using TrackService;
 
+    [RoutePrefix("api/Rastreios")]
     public class RastreiosController : ApiController
     {
+
         private CorreiosTrackContext db = new CorreiosTrackContext();
 
+        private readonly RastreioService _service;
 
-
-        
-
-
+        public RastreiosController()
+        {
+            _service = new RastreioService();
+        }
 
         // GET: api/Rastreios
         public IQueryable<Rastreio> GetRastreios()
         {
-            return db.Rastreios;
+            return _service.GetRastreios();
+        }
+
+        //Example of custom routing for test purposes
+        [Route("test/{id}")]
+        [HttpGet]
+        public IHttpActionResult GetSomething(long id)
+        {
+            return Ok(_service.GetSomething(2));
         }
 
         // GET: api/Rastreios/5
         [ResponseType(typeof(Rastreio))]
         public IHttpActionResult GetRastreio(long id)
         {
-            Rastreio rastreio = db.Rastreios.Find(id);
+            Rastreio rastreio = _service.GetRastreio(id);
             if (rastreio == null)
             {
                 return NotFound();
@@ -59,27 +68,14 @@ namespace CorreiosTrack.Controllers
                 return BadRequest();
             }
 
-            string trackStatus = ConsultTrackStatus(rastreio.Codigo);
+            string trackStatus = _service.ConsultTrackStatus(rastreio.Codigo);
             rastreio.Status = trackStatus;
 
-            db.Entry(rastreio).State = EntityState.Modified;
-
-            try
+            bool result = _service.PutRastreio(id, rastreio);
+            if (!result)
             {
-                db.SaveChanges();
+                return NotFound();
             }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!RastreioExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
             return StatusCode(HttpStatusCode.NoContent);
         }
 
@@ -92,11 +88,10 @@ namespace CorreiosTrack.Controllers
                 return BadRequest(ModelState);
             }
 
-            string trackStatus = ConsultTrackStatus(rastreio.Codigo);
+            string trackStatus = _service.ConsultTrackStatus(rastreio.Codigo);
             rastreio.Status = trackStatus;
 
-            db.Rastreios.Add(rastreio);
-            db.SaveChanges();
+            _service.PostRastreio(rastreio);
 
             return CreatedAtRoute("DefaultApi", new { id = rastreio.Id }, rastreio);
         }
@@ -105,89 +100,15 @@ namespace CorreiosTrack.Controllers
         [ResponseType(typeof(Rastreio))]
         public IHttpActionResult DeleteRastreio(long id)
         {
-            Rastreio rastreio = db.Rastreios.Find(id);
+            Rastreio rastreio = _service.DeleteRastreio(id);
             if (rastreio == null)
             {
                 return NotFound();
             }
-
-            db.Rastreios.Remove(rastreio);
-            db.SaveChanges();
-
             return Ok(rastreio);
         }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
 
-        private bool RastreioExists(long id)
-        {
-            return db.Rastreios.Count(e => e.Id == id) > 0;
-        }
-
-        private string parseXML(string xml)
-        {
-            string result = "";
-            int initIndexDesc = xml.IndexOf("<descricao>") + "<descricao>".Length;
-            int finalLenghtDesc = xml.IndexOf("</descricao>") - initIndexDesc;
-            result += xml.Substring(initIndexDesc, finalLenghtDesc);
-            if (xml.Contains("<destino>"))
-            {
-                int initIndexLocal = xml.IndexOf("<local>", xml.IndexOf("<local>") + 1) + "<local>".Length;
-                int finalLenghtLocal = xml.IndexOf("</local>", xml.IndexOf("</local>") + 1) - initIndexLocal;
-                result += "para " + xml.Substring(initIndexLocal, finalLenghtLocal);
-            }
-            return result;
-        }
-
-        private static HttpWebRequest CreateWebRequest()
-        {
-            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(@"http://webservice.correios.com.br/service/rastro");
-            webRequest.Headers.Add("SOAPAction", "http://webservice.correios.com.br/service/rastro/Rastro");
-            webRequest.ContentType = "text/xml;charset=\"utf-8\"";
-            webRequest.Accept = "text/xml";
-            webRequest.Method = "POST";
-            return webRequest;
-        }
-
-        private string ConsultTrackStatus(string trackCode)
-        {
-            HttpWebRequest request = CreateWebRequest();
-            XmlDocument soapEnvelopeXml = new XmlDocument();
-            soapEnvelopeXml.LoadXml(@"<soapenv:Envelope xmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/""
-            xmlns:res=""http://resource.webservice.correios.com.br/"">
-            <soapenv:Header/>
-            <soapenv:Body>
-            <res:buscaEventos>
-            <usuario>ECT</usuario>
-            <senha>SRO</senha>
-            <tipo>L</tipo>
-            <resultado>T</resultado>
-            <lingua>101</lingua>
-            <objetos>" + trackCode + @"</objetos>
-            </res:buscaEventos>
-            </soapenv:Body>
-            </soapenv:Envelope>");
-
-            using (Stream stream = request.GetRequestStream())
-            {
-                soapEnvelopeXml.Save(stream);
-            }
-
-            using (WebResponse response = request.GetResponse())
-            {
-                using (StreamReader rd = new StreamReader(response.GetResponseStream()))
-                {
-                    string soapResult = rd.ReadToEnd();
-                    return parseXML(soapResult);
-                }
-            }
-        }
+       
     }
 }
---
+++ CTBack/CorreiosTrack/CorreiosTrack/Services/RastreioService.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CorreiosTrack.Models;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Net;
+using System.Xml;
+using System.IO;
+
+namespace CorreiosTrack.Services
+{
+    public class RastreioService
+    {
+        private CorreiosTrackContext db = new CorreiosTrackContext();
+
+        public IQueryable<Rastreio> GetRastreios()
+        {
+            return db.Rastreios;
+        }
+
+        public Rastreio GetRastreio(long id)
+        {
+            return db.Rastreios.Find(id);
+        }
+
+        public bool PutRastreio(long id, Rastreio rastreio)
+        {
+            db.Entry(rastreio).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RastreioExists(id))
+                {
+                    return false;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return true;
+        }
+
+        public void PostRastreio(Rastreio rastreio)
+        {
+            db.Rastreios.Add(rastreio);
+            db.SaveChanges();
+        }
+
+        public Rastreio DeleteRastreio(long id)
+        {
+            Rastreio rastreio = db.Rastreios.Find(id);
+            if (rastreio == null)
+            {
+                return null;
+            }
+
+            db.Rastreios.Remove(rastreio);
+            db.SaveChanges();
+
+            return rastreio;
+        }
+
+        public List<Rastreio> GetSomething(long id)
+        {
+            //Example using LINQ for test purposes
+            using (var context = new CorreiosTrackContext())
+            {
+                context.Rastreios.Load();
+                var query = (from r in context.Rastreios.Local where r.Status.Contains("encaminhado") select r).ToList();
+                return query;
+            }
+        }
+
+        private bool RastreioExists(long id)
+        {
+            return db.Rastreios.Count(e => e.Id == id) > 0;
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (db != null)
+                {
+                    db.Dispose();
+                    db = null;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private string parseXML(string xml)
+        {
+            string result = "";
+            int initIndexDesc = xml.IndexOf("<descricao>") + "<descricao>".Length;
+            int finalLenghtDesc = xml.IndexOf("</descricao>") - initIndexDesc;
+            result += xml.Substring(initIndexDesc, finalLenghtDesc);
+            if (xml.Contains("<destino>"))
+            {
+                int initIndexLocal = xml.IndexOf("<local>", xml.IndexOf("<local>") + 1) + "<local>".Length;
+                int finalLenghtLocal = xml.IndexOf("</local>", xml.IndexOf("</local>") + 1) - initIndexLocal;
+                result += "para " + xml.Substring(initIndexLocal, finalLenghtLocal);
+            }
+            return result;
+        }
+
+        private static HttpWebRequest CreateWebRequest()
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(@"http://webservice.correios.com.br/service/rastro");
+            webRequest.Headers.Add("SOAPAction", "http://webservice.correios.com.br/service/rastro/Rastro");
+            webRequest.ContentType = "text/xml;charset=\"utf-8\"";
+            webRequest.Accept = "text/xml";
+            webRequest.Method = "POST";
+            return webRequest;
+        }
+
+        public string ConsultTrackStatus(string trackCode)
+        {
+            HttpWebRequest request = CreateWebRequest();
+            XmlDocument soapEnvelopeXml = new XmlDocument();
+            soapEnvelopeXml.LoadXml(@"<soapenv:Envelope xmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/""
+            xmlns:res=""http://resource.webservice.correios.com.br/"">
+            <soapenv:Header/>
+            <soapenv:Body>
+            <res:buscaEventos>
+            <usuario>ECT</usuario>
+            <senha>SRO</senha>
+            <tipo>L</tipo>
+            <resultado>T</resultado>
+            <lingua>101</lingua>
+            <objetos>" + trackCode + @"</objetos>
+            </res:buscaEventos>
+            </soapenv:Body>
+            </soapenv:Envelope>");
+
+            using (Stream stream = request.GetRequestStream())
+            {
+                soapEnvelopeXml.Save(stream);
+            }
+
+            using (WebResponse response = request.GetResponse())
+            {
+                using (StreamReader rd = new StreamReader(response.GetResponseStream()))
+                {
+                    string soapResult = rd.ReadToEnd();
+                    return parseXML(soapResult);
+                }
+            }
+        }
+    }
+}
